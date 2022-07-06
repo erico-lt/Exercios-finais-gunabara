46d6fb4eba60392db8c5b3e7a7ed01c216ad5d9d
erico-lt <erico.blp@gmail.com>
2022-07-05 21:39:31 -0300

    Criança das classes Gafanhoto e Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    protected String nome;
+    protected String sexo;
+    protected String experiencia;
+    protected int idade;
+
+    protected void ganharExp(){
+        
+    }    
+}
